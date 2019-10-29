aab5e4bbffa0703b6d461d8188f05b4ecf3ee9ed
themuzic <53420470+themuzic@users.noreply.github.com>
2019-10-29 13:49:01 +0900

    품앗이 vo, mapper 세팅
    ㅇㅇ

---
+++ src/main/java/com/kh/develoffice/todo/model/vo/Help.java
@@ -0,0 +1,26 @@
+package com.kh.develoffice.todo.model.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Help {
+	
+	private int workId;
+	private int empId;
+	private String workTitle;
+	private String workContent;
+	private String registDate;
+	private int helperId;
+	private String status;
+	
+
+}
